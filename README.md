f636aa547fea60a717c4f6286ee6c44b0ffc8f74
Justin Lin <caterpillar@openhome.cc>
2015-07-09 16:03:43 +0800

    搞錯了馬達方向，修正…XD

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 [![廢材四足機器人](picture.jpg)](https://www.youtube.com/watch?v=d2s3qi_liVk)
 
-用飛機木、冰棒棍、竹筷修修補補而成。
+用飛機木、冰棒棍、竹筷、美工刀、保麗龍膠修修補補而成，程式還在撰寫中 …
 
 - [切割紙型檔案](Hexapod.pdf)
 - [Arduino 原始碼](src/Hexapod/Hexapod.ino) 
--- src/Hexapod/Hexapod.ino
+++ src/Hexapod/Hexapod.ino
@@ -13,26 +13,43 @@ int delayMillis = 1;
 
 void setup() {
   initServos();  
-  upTest(1);
+//  
+//  leg1UpTest(1);
+//  leg1UpTest(-1);
+//  leg2UpTest(1);
+//  leg2UpTest(-1);
+//  leg3UpTest(1);
+//  leg3UpTest(-1);
+//  leg4UpTest(1);
+//  leg4UpTest(-1);  
+//  
+  upTest(1);  
+//  
+//  clockwiseTest(1);  
+//  clockwiseTest(-1);
+//  clockwiseTest(-1);
+//  clockwiseTest(1);  
 }
 
 void loop() {
-//  clockWiseTest(-1);
-//  forerakeTest(1);
-//  forerakeTest(-1);
-//  upTest(-1);
+  for(int i = 0; i < 3; i++) { // up down 3 times
+    upTest(1); upTest(-1);    
+  }
+  for(int i = 0; i < 3; i++) {  // lift legs 3 times
+    leg1UpTest(1);  leg1UpTest(-1);
+    leg2UpTest(1);  leg2UpTest(-1);
+    leg3UpTest(1);  leg3UpTest(-1);
+    leg4UpTest(1);  leg4UpTest(-1);    
+  }
+  
+  for(int i = 0; i < 3; i++) {  // turn right 3 times
+    turnRightTest(1);
+  }
 
-  turnRightTest(1);
-  turnRightTest(1);
-  turnRightTest(1);
-  turnRightTest(1);
-  turnRightTest(1);
-
-  turnRightTest(-1);
-  turnRightTest(-1);
-  turnRightTest(-1);
-  turnRightTest(-1);
-  turnRightTest(-1);  
+  
+  for(int i = 0; i < 3; i++) {  // turn left 3 times
+    turnRightTest(-1);
+  }
 }
 
 void initServos() {
@@ -69,42 +86,70 @@ void leg4_1Clockwise(int deg) {
   degs[1][0] += deg;
 }
 
-void leg1_2Up(int deg) {
+void leg1_2Down(int deg) {
   degs[3][1] += deg;
 }
 
-void leg2_2Up(int deg) {
+void leg2_2Down(int deg) {
   degs[2][1] += deg;
 }
 
-void leg3_2Up(int deg) {
+void leg3_2Down(int deg) {
   degs[0][1] -= deg;
 }
 
-void leg4_2Up(int deg) {
+void leg4_2Down(int deg) {
   degs[1][1] -= deg;
 }
 
-void leg1_3Up(int deg) {
+void leg1_3Down(int deg) {
   degs[3][2] += deg;
 }
 
-void leg2_3Up(int deg) {
+void leg2_3Down(int deg) {
   degs[2][2] += deg;
 }
 
-void leg3_3Up(int deg) {
+void leg3_3Down(int deg) {
   degs[0][2] -= deg;
 }
 
-void leg4_3Up(int deg) {
+void leg4_3Down(int deg) {
   degs[1][2] -= deg;
+}
+
+void leg1UpTest(int deg) {
+  for(int i = 0; i < 45; i++) {
+    leg1_2Down(deg); leg1_3Down(deg); 
+    updateServos();
+  }  
+}
+
+void leg2UpTest(int deg) {
+  for(int i = 0; i < 45; i++) {
+    leg2_2Down(deg); leg2_3Down(deg); 
+    updateServos();
+  }  
+}
+
+void leg3UpTest(int deg) {
+  for(int i = 0; i < 45; i++) {
+    leg3_2Down(deg); leg3_3Down(deg); 
+    updateServos();
+  }  
+}
+
+void leg4UpTest(int deg) {
+  for(int i = 0; i < 45; i++) {
+    leg4_2Down(deg); leg4_3Down(deg); 
+    updateServos();
+  }  
 }
 
 void upTest(int deg) {
   for(int i = 0; i < 45; i++) {
-    leg1_2Up(deg); leg2_2Up(deg); leg3_2Up(deg); leg4_2Up(deg);
-    leg1_3Up(deg); leg2_3Up(deg); leg3_3Up(deg); leg4_3Up(deg);
+    leg1_2Down(deg); leg2_2Down(deg); leg3_2Down(deg); leg4_2Down(deg);
+    leg1_3Down(deg); leg2_3Down(deg); leg3_3Down(deg); leg4_3Down(deg);
     updateServos();
   }  
 }
@@ -151,23 +196,36 @@ void turnRightTest(int deg) {
     leg4_1Clockwise(-deg);
     updateServos();
   }
-  for(int i = 0; i < 45; i++) {
-      leg1_1Clockwise(deg);
+  // leg one by one
+  int multipleDeg = deg * 2;
+  int absMultipleDeg = abs(multipleDeg);
+  
+  leg1_2Down(-15); leg1_3Down(-15); 
+  for(int i = 0; i < 45; i += absMultipleDeg) {
+      leg1_1Clockwise(multipleDeg);
       updateServos();
   }  
-  for(int i = 0; i < 45; i++) {
-      leg3_1Clockwise(deg);
-      updateServos();
-  }  
+  leg1_2Down(15); leg1_3Down(15); 
 
-  for(int i = 0; i < 45; i++) {
-      leg2_1Clockwise(deg);
+  leg3_2Down(-15); leg3_3Down(-15);   
+  for(int i = 0; i < 45; i += absMultipleDeg) {
+      leg3_1Clockwise(multipleDeg);
       updateServos();
   }  
-    
-  for(int i = 0; i < 45; i++) {
-      leg4_1Clockwise(deg);
+  leg3_2Down(15); leg3_3Down(15); 
+  
+  leg2_2Down(-15); leg2_3Down(-15); 
+  for(int i = 0; i < 45; i += absMultipleDeg) {
+      leg2_1Clockwise(multipleDeg);
       updateServos();
-  }      
+  } 
+  leg2_2Down(15); leg2_3Down(15);  
+    
+  leg4_2Down(-15); leg4_3Down(-15); 
+  for(int i = 0; i < 45; i += absMultipleDeg) {
+      leg4_1Clockwise(multipleDeg);
+      updateServos();
+  } 
+  leg4_2Down(15); leg4_3Down(15);   
 }
 
